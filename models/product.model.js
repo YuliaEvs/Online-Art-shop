ec7b9c12eb747fc2b513294b55f096b1a6583510
Julesnorules <goshootingforthestars@gmail.com>
2023-06-21 14:28:12 -0400

    finished

--- models/product.model.js
+++ models/product.model.js
@@ -45,6 +45,22 @@ class Product {
     });
   }
 
+  static async findMultiple(ids) {
+    const productIds = ids.map(function(id) {
+      return new mongodb.ObjectId(id);
+    })
+    
+    const products = await db
+      .getDb()
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray();
+
+    return products.map(function (productDocument) {
+      return new Product(productDocument);
+    });
+  }
+
   updateImageData() {
     this.imagePath = `product-data/images/${this.image}`;
     this.imageUrl = `/products/assets/images/${this.image}`;
@@ -86,7 +102,6 @@ class Product {
     const productId = new mongodb.ObjectId(this.id);
     return db.getDb().collection('products').deleteOne({ _id: productId });
   }
-  
 }
 
 module.exports = Product;
